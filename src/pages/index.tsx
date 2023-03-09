de5cf711a1b9e97fb5e75303f73a14b7739ed306
Fernando sosa <fsosat1400@alumno.ipn.mx>
2023-03-08 18:47:57 -0600

    Add the card of the task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { Layout } from "@/components/layouts";
 import { UIProvider } from "@/context/ui";
 
@@ -10,10 +6,31 @@ export default function Home() {
   return (
     <>
       <UIProvider>
-        <Layout title="hola mundo">
-          <Typography variant="h1" color="primary">
-            hello
-          </Typography>
+        <Layout title="Home openJira">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ height: "calc(100vh - 100px)" }}>
+                <CardHeader title="Pendientes">
+                  <CardContent>
+                    {/* Agregar una nueva Entrada */}
+                    {/* Listado de las entradas */}
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ height: "calc(100vh - 100px)" }}>
+                <CardHeader title="En progreso"></CardHeader>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Card sx={{ height: "calc(100vh - 100px)" }}>
+                <CardHeader title="Completadas"></CardHeader>
+              </Card>
+            </Grid>
+          </Grid>
         </Layout>
       </UIProvider>
     </>
