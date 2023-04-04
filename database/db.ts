28db20c7f0b397c8acdb977db8f0b7eda5ad3e12
Fernando sosa <fsosat1400@alumno.ipn.mx>
2023-04-03 21:09:09 -0600

    Add the docker and the connection

---
+++ database/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+// 0 disconnected
+// 1 connected
+// 2 connecting
+// 3 disconnecting
+
+const mongooConnection = {
+  isConnected: 0,
+};
+
+const connect = async () => {
+  if (mongooConnection.isConnected) {
+    console.log("Conectados");
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+    if (mongooConnection.isConnected === 1) {
+      console.log("Usando conexion anterior");
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  await mongoose.connect("...");
+  mongooConnection.isConnected = 1;
+  console.log("Conectadoo a mongo db", "");
+};
+
+export const disconnect = async () => {
+  if (mongooConnection.isConnected !== 0) return;
+  await mongoose.disconnect();
+  console.log("Desconectado");
+};
